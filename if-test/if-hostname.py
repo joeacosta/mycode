b737af12b9592024ff1fbb0f5be9d6f3bd1482f2
Joe Acosta <joe.acosta@verizonwireless.com>
2018-12-04 11:46:26 -0500

    hostname

---
+++ if-test/if-hostname.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#hostname
+hostname = 'MTG'
+if hostname == 'MTG', 'mtg', 'mTg', 'MTg', 'mTG', 'MtG',:
+	print('The hostname was found to be mtg')
+print (hostname matches expected config)
--- mix-list3/mix-list3.py
+++ mix-list3/mix-list3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# mix3
 list1 = ['cisco_nxos', 'arista_eos', 'cisco_ios']
 print(list1)
 list1.extend(['juniper'])
